ffd9014d976132bfb972da2a1d5e9156765c6b91
Amir <amirsadik636@gmail.com>
2024-09-07 06:57:18 -0700

    Printing a famous quote

---
+++ python_crash_course/02-ch/2-5_quote.py
@@ -0,0 +1,17 @@
+"""
+	2-5. Famous Quote
+
+	Find a quote from a famous person you admire. Print the quote and the name of its author. 
+	Your output should look something like the following, including the quotation marks:
+
+    	Albert Einstein once said, "person who never made a mistake never tried anything new."
+"""
+
+# create a variable for the name
+name = 'Albert Einstein'
+
+# create a variable for the quote
+quote = 'person who never made a mistake never tried anything new.'
+
+# print the name and the quote using f-string
+print(f'{name} once said, "{quote}"')
