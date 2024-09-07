cc75ae768812c581d60ca1d55ec8744d2c0ec2ca
Amir <amirsadik636@gmail.com>
2024-09-07 07:34:34 -0700

    printng without extensions

---
+++ python_crash_course/02-ch/2-8_extensions.py
@@ -0,0 +1,16 @@
+"""
+	2-8. File Extensions
+    
+	Python has a removesuffix() method that works exactly like
+	removeprefix(). Assign the value 'python_notes.txt' to a
+	variable called filename. Then use the removesuffix() method
+	to display the filename without the file extension, like some
+	file browsers do.
+
+"""
+
+# create the variable for the file name
+filename = 'python_notes.txt'
+
+# print the file name without the file extention
+print(filename.removesuffix('.txt'))
