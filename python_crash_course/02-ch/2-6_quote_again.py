364c9e6105188278abc86bbd8f98c4e3a9450c09
Amir <amirsadik636@gmail.com>
2024-09-07 07:10:46 -0700

    Printing a famous quote again

---
+++ python_crash_course/02-ch/2-6_quote_again.py
@@ -0,0 +1,16 @@
+"""
+	2-6. Famous Quote 2
+
+	Repeat Exercise 2-5, but this time, represent the famous person's name using
+	a variable called famous_person. Then compose your message and represent it
+	with a new variable called message. Print your message.
+"""
+
+# create a variable for the name
+famous_person = 'Malcolm X'
+
+# create a variable for the quote
+message = 'The future belongs to those who prepare for it today.'
+
+# print the name and the quote using f-string
+print(f'{famous_person} once said, "{message}"')
