6607a1c6e90ac77b5f8fa96aea6935dd4832655d
Amir <amirsadik636@gmail.com>
2024-09-22 19:39:31 +0300

    Update 01-age_checker.py

--- learn_to_program/01-age_checker.py
+++ learn_to_program/01-age_checker.py
@@ -2,8 +2,9 @@
 	age_checker.py
 
 	Ask the user for their age. For ages below 4, tell they're too early to go to school.
-	For ages 4 to 6 print go to kindergarten. For ages 7 to 18 tell to go the grade 
-	level that is less than from their age by 6. For ages above 18 tell'em to go to collage.
+	For ages 4 to 6 print go to kindergarten. For ages 7 to 18 tell to go to the grade 
+	level that is less than from their age by 6 (For age 10, tell to go to grade 4).
+	For ages above 18 tell'em to go to collage.
 
 '''
 # Ask the user for their age and put it in a variable
