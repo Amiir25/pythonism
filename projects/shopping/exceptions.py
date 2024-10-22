1c43f5f07c567f038d39bdb402c95c206e06ba3e
Amir <amirsadik636@gmail.com>
2024-10-22 17:20:56 +0300

    Add exceptions.py

---
+++ projects/shopping/exceptions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+exceptions.py
+
+This file custome exception classes
+"""
+
+
+class InvalidPriceError(Exception):
+    """Custome exception class for invalid price"""
+
+    pass
+
+class InvalidStockError(Exception):
+    """Custome exception class for invalid stock"""
+
+    pass
