4c8f6c44e0216c65ed009778d975dbdf9a01e5db
Amir <amirsadik636@gmail.com>
2024-10-22 17:20:18 +0300

    Update product.py

--- projects/shopping/product.py
+++ projects/shopping/product.py
@@ -6,6 +6,8 @@ product.py
 This file provides a Product class
 """
 
+import exceptions
+
 
 class Product:
     """The product class"""
@@ -37,7 +39,7 @@ class Product:
         """Setter method for __price attribute"""
 
         if not isinstance(price, (int, float)) or price <= 0:
-            raise Exception("Invalid price! Must be a positive number")
+            raise exceptions.InvalidPriceError("Invalid price! Must be a positive number")
 
         self.__price = price
     @property
@@ -51,6 +53,19 @@ class Product:
         """Setter method for __stock attribute"""
 
         if not isinstance(stock, int) or stock < 0:
-            raise Exception("Invalid stock! Must be a non-negative integer")
+            raise exceptions.InvalidStockError("Invalid stock! Must be a non-negative integer")
 
         self.__stock = stock
+
+    def is_in_stock(self):
+        """Checks if the product is in stock"""
+
+        return self.stock > 0
+
+    def add_stock(self, amount):
+        """Adds a stock for a product"""
+
+        if amount <= 0:
+            raise InvalidStockError("Amount to add must be non-negative")
+
+        self.stock += amount
