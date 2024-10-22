800669f28854ed29d1ea0b5e08e3f75ad57079a4
Amir <amirsadik636@gmail.com>
2024-10-22 17:28:29 +0300

    Add test_product.py

---
+++ projects/shopping/tests/test_product.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+test_product.py
+
+A Unit test file
+"""
+
+import unittest
+from product import Product
+
+
+class TestProduct(unittest.TestCase):
+
+    def test_costructor(self):
+        """Test the 
