8aebbc5d430d91a1973e33bf3fd85f419e63c35d
Amir <amirsadik636@gmail.com>
2024-10-23 19:00:27 +0300

    Update product.py

--- projects/shopping/product.py
+++ projects/shopping/product.py
@@ -31,8 +31,8 @@ class Product:
     def __repr__(self):
         """Returns a developer-friendly representation of an object"""
 
-        return "Product(name={}, price={}, stock={}).
-                format(self.name, self.price, self.stock)"
+        return "Product(name={}, price={}, stock={}").
+                format(self.name, self.price, self.stock)
 
     @property
     def price(self):
