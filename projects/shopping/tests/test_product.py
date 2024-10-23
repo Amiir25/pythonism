3a0698de23c1e46710d23a76b10a822f6c1bcd1d
Amir <amirsadik636@gmail.com>
2024-10-23 19:17:56 +0300

    Update tests/

--- projects/shopping/tests/test_product.py
+++ projects/shopping/tests/test_product.py
@@ -12,3 +12,12 @@ from product import Product
 
 class TestProduct(unittest.TestCase):
 
+    def test_constructor(self):
+        """Tests the constructor"""
+
+        p1 = Product("Book", 12.55, "Educational Book", 21)
+        self.assertEqual(p1, Product(name=Book, price=12.55, stock=21))
+
+
+if __name__ == "__main__":
+    unittest.main()
