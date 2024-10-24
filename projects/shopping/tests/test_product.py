6e62e5571b487d92dfd6c9d290c203f9d9c5f5be
Amir <amirsadik636@gmail.com>
2024-10-24 14:39:45 +0300

    Update tests/test_product.py

--- projects/shopping/tests/test_product.py
+++ projects/shopping/tests/test_product.py
@@ -17,6 +17,10 @@ class TestProduct(unittest.TestCase):
         """Tests the constructor"""
 
         p1 = Product("B", 12.55, "Book", 21)
+        self.assertEqual(p1.name, "B")
+        self.assertEqual(p1.price, 12.55)
+        self.assertEqual(p1.description, "Book")
+        self.assertEqual(p1.stock, 21)
         self.assertEqual(p1, Product(name="B", price=12.55, description="Book", stock=21))
 
 
