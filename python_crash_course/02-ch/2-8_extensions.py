ad56b4e0eb99e9433092c072f8fc02b377475622
Amir <amirsadik636@gmail.com>
2025-01-04 20:19:41 +0300

    Update 2-8_extensions.py

--- python_crash_course/02-ch/2-8_extensions.py
+++ python_crash_course/02-ch/2-8_extensions.py
@@ -1,12 +1,11 @@
 """
-	2-8. File Extensions
+2-8. File Extensions
     
-	Python has a removesuffix() method that works exactly like
-	removeprefix(). Assign the value 'python_notes.txt' to a
-	variable called filename. Then use the removesuffix() method
-	to display the filename without the file extension, like some
-	file browsers do.
-
+Python has a removesuffix() method that works exactly like
+removeprefix(). Assign the value 'python_notes.txt' to a
+variable called filename. Then use the removesuffix() method
+to display the filename without the file extension, like some
+file browsers do.
 """
 
 # create the variable for the file name
