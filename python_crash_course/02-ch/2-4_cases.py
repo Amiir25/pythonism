3ceb5e424a480dd615e028540b2947aa8459e606
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-03-10 22:28:21 +0300

    Update 2-4_cases.py

--- python_crash_course/02-ch/2-4_cases.py
+++ python_crash_course/02-ch/2-4_cases.py
@@ -2,7 +2,7 @@
 2-4. Name Cases
 
 Use a variable to represent a person's name, and then
-	print that person's name in lowercase, uppercase, and title case.
+print that person's name in lowercase, uppercase, and title case.
 """
 
 # define a variable & assign a name
