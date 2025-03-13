e0ec1c86c86d2bbc42890238fab2f081a27acb48
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-03-13 21:43:19 +0300

    Update 2-3_message.py

--- python_crash_course/02-ch/2-3_message.py
+++ python_crash_course/02-ch/2-3_message.py
@@ -5,5 +5,6 @@ Use a variable to represent a person's name and print a message to
 that person. Your message should be simple such as, "Hello Eric, would
 you like to learn some Python today?"
 '''
+
 name = 'Tedy'
 print(f'Hello {name}, Would you like to learn some Python today?')
