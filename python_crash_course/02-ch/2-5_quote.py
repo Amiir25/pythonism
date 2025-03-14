03dd4f09bd27be60568e7a9a1d86cea5c775efd1
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-03-14 18:07:55 +0300

    Update 2-5_quote.py

--- python_crash_course/02-ch/2-5_quote.py
+++ python_crash_course/02-ch/2-5_quote.py
@@ -1,7 +1,7 @@
 """
 2-5. Famous Quote
 
-	Find a quote from a famous person you admire. Print the quote and the name of its author. 
+Find a quote from a famous person you admire. Print the quote and the name of its author. 
 	Your output should look something like the following, including the quotation marks:
 
     	Albert Einstein once said, "person who never made a mistake never tried anything new."
