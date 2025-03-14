5ca97e6ccf615a71f2686786035a56909a39c99e
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-03-14 18:08:21 +0300

    Update 2-5_quote.py

--- python_crash_course/02-ch/2-5_quote.py
+++ python_crash_course/02-ch/2-5_quote.py
@@ -4,7 +4,7 @@
 Find a quote from a famous person you admire. Print the quote and the name of its author. 
 Your output should look something like the following, including the quotation marks:
 
-    	Albert Einstein once said, "person who never made a mistake never tried anything new."
+Albert Einstein once said, "person who never made a mistake never tried anything new."
 """
 
 print('Malcolm X once said "The future belongs to those who prepare for it today."')
