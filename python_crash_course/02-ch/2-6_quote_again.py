b980b6d3222e9c702c04f13e29beb4a3d069471d
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-03-15 22:09:17 +0300

    Update 2-6_quote_again.py

--- python_crash_course/02-ch/2-6_quote_again.py
+++ python_crash_course/02-ch/2-6_quote_again.py
@@ -3,7 +3,7 @@
 
 Repeat Exercise 2-5, but this time, represent the famous person's name using
 a variable called famous_person. Then compose your message and represent it
-	with a new variable called message. Print your message.
+with a new variable called message. Print your message.
 """
 
 # create a variable for the name
