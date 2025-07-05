2feb61a3cf9b815a4ce2ce082a999c809ce8aad9
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-07-05 11:16:40 +0300

    Update 03-tree_height.py

--- learn_to_program/03-tree_height.py
+++ learn_to_program/03-tree_height.py
@@ -2,7 +2,7 @@
 02-tree_size.py
 
 Ask the user for a number and print a tree like shape using the
-# sign by the size of the input. This an example out put for input 
+# sign by the size of the input. This is the example out put for input 
 value of 5.
 
 		#
