cc0213f24bf9a4c44e663658e7ded1bbfbea5ad5
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-07-07 18:48:00 +0300

    Update 2-7_stripping.py

--- python_crash_course/02-ch/2-7_stripping.py
+++ python_crash_course/02-ch/2-7_stripping.py
@@ -2,7 +2,7 @@
 2-7. Stripping Names
 
 Use a variable to represent a person's name, and include some 
-whitespace characters at the beginning and end of the name.
+whitespace characters at the beginning and at the end of the name.
 Make sure you use each character combination, "\t" and "\n",
 at least once. Print the name once, so the whitespace around
 the name is displayed. Then print the name using each of the
